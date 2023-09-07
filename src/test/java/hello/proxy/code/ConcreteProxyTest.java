c95a73f8ce383c93c3addf165187201f9b82ff04
Dev-suho <devsuho99@gmail.com>
2023-09-08 06:41:55 +0900

    ConcreteClassProxy_0908

--- src/test/java/hello/proxy/code/ConcreteProxyTest.java
+++ src/test/java/hello/proxy/code/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.code;
 
 import hello.proxy.code.concreteproxy.ConcreteClient;
 import hello.proxy.code.concreteproxy.ConcreteLogic;
+import hello.proxy.code.concreteproxy.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -12,4 +13,13 @@ public class ConcreteProxyTest {
         ConcreteClient client = new ConcreteClient(concreteLogic);
         client.execute();
     }
+
+    @Test
+    void timeProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+    }
+
 }
