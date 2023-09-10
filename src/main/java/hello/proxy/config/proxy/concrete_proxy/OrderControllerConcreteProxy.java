41b5896a121aa5613a87a6793b8e0c7a25c2b212
Dev-suho <devsuho99@gmail.com>
2023-09-11 00:14:47 +0900

    ConcreteProxy_0911

---
+++ src/main/java/hello/proxy/config/proxy/concrete_proxy/OrderControllerConcreteProxy.java
@@ -0,0 +1,37 @@
+package hello.proxy.config.proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderControllerConcreteProxy extends OrderControllerV2 {
+
+    private final OrderControllerV2 orderController;
+    private final LogTrace trace;
+
+    public OrderControllerConcreteProxy(OrderControllerV2 orderController, LogTrace trace) {
+        super(null);
+        this.orderController = orderController;
+        this.trace = trace;
+    }
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            String result = orderController.request(itemId);
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    @Override
+    public String noLog() {
+        return orderController.noLog();
+    }
+}
