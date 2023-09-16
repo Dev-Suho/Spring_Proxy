55ff4880c24c07dc3b2325f1aee06d9e91a44671
Dev-suho <devsuho99@gmail.com>
2023-09-16 20:27:15 +0900

    Refactoring_0916

--- src/main/java/hello/proxy/app/v3/OrderRepositoryV3.java
+++ src/main/java/hello/proxy/app/v3/OrderRepositoryV3.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class OrderRepositoryV3 {
     public void save(String itemId) {
-        if (itemId.equals("ex")) {
+        if (itemId.equals("exception")) {
             throw new IllegalStateException("예외 발생");
         }
         sleep(1000);
